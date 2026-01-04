324d909542f23c5c855caa8e96fff6aa5b2f252f
Sushil Kumar <sushilkumbhar77@gmail.com>
2026-01-04 18:09:06 +0530

    states api

---
+++ data.php
@@ -0,0 +1,115 @@
+<?php
+$myArray = [
+    "Continets" => [
+        "Asia" => [
+            "country" => [
+                "Afghanistan",
+            "Armenia",
+            "Azerbaijan",
+            "Bahrain",
+            "Bangladesh",
+            "Bhutan",
+            "Brunei",
+            "Cambodia",
+            "China",
+            "Cyprus",
+            "Egypt",
+            "Georgia",
+            "india" => [
+                "state" => [
+                    "Andhra Pradesh",
+                    "Arunachal Pradesh",
+                    "Assam",
+                    "Bihar",
+                    "Chhattisgarh",
+                    "Goa",
+                    "Gujarat",
+                    "Haryana",
+                    "Himachal Pradesh",
+                    "Jharkhand",
+                    "Karnataka",
+                    "Kerala",
+                    "Madhya Pradesh",
+                    "Maharashtra",
+                    "Manipur",
+                    "Meghalaya",
+                    "Mizoram",
+                    "Nagaland",
+                    "odisha" => [
+                        "district" => [
+                            "Bargarh",
+                            "Anugul"
+                        ]
+                    ],
+                    "Punjab" => [
+                        "district" => [
+                            "hisar"
+                        ]
+                    ],
+                    "Rajasthan",
+                    "Sikkim",
+                    "Tamil Nadu",
+                    "Telangana",
+                    "Tripura",
+                    "Uttar Pradesh",
+                    "Uttarakhand",
+                    "West Bengal",
+                    "Union Territories" => [
+                        "Andaman and Nicobar Islands",
+                        "Chandigarh",
+                        "Dadra and Nagar Haveli and Daman and Diu",
+                        "Delhi",
+                        "Jammu and Kashmir",
+                        "Ladakh",
+                        "Lakshadweep",
+                        "Puducherry"
+                    ]
+                ]
+            ],
+            "Indonesia",
+            "Iran",
+            "Iraq",
+            "Israel",
+            "Japan",
+            "Jordan",
+            "Kazakhstan",
+            "Kuwait",
+            "Kyrgyzstan",
+            "Laos",
+            "Lebanon",
+            "Malaysia",
+            "Maldives",
+            "Mongolia",
+            "Myanmar",
+            "Nepal",
+            "North Korea",
+            "Oman",
+            "Pakistan" => [
+                "state" => [
+                    "Boluchistan" => [
+                        "district" => [
+                            "karachi"
+                        ]
+                    ]
+                ]
+            ],
+            "Philippines",
+            "Qatar",
+            "Russia",
+            "Saudi Arabia",
+            "Singapore",
+            "South Korea",
+            "Sri Lanka",
+            "Syria",
+            "Tajikistan",
+            "Thailand",
+            "Timor-Leste",
+            "Turkey",
+            "Turkmenistan",
+            "United Arab Emirates",
+            "Uzbekistan",
+            "Vietnam",
+            "Yemen"
+        ]
+    ]]
+];
