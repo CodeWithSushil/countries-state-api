16b58507b0f4f7ca44c4f7774d9fb97730f1fb6d
Sushil Kumar <sushilkumbhar77@gmail.com>
2026-01-05 11:55:45 +0530

    update

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-fetch("./data/Asia_Countries_States.json").then(response => {
+const asia = "./data/asia.json";
+const india = "./data/india.json";
+const conti = "./data/continets.json";
+
+fetch(asia).then(response => {
     if (!response.ok) {
         throw new Error("HTTP error " + response.status);
     }
